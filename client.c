c16a9a71f069060510b45c6d60c8cf078ca40d80
gabriele0011 <99204202+gabriele0011@users.noreply.github.com>
2022-03-02 15:16:26 +0100

    implementazione visit_folder_send_request

--- client.c
+++ client.c
@@ -5,13 +5,17 @@
 int flag_D = 0;
 int flag_d = 0;
 int flag_p = 0;
-int time_r;
+int flag_rR = 0;
+int flag_wW = 0;
+int time_r = 0;
 int flag_h = 0;
 char *socket_name = NULL; //controllo memoria
-
+char* arg_D = NULL;
+char* arg_d = NULL;
 
 ////////////////// ISNUMBER //////////////////
-long isNumber(const char* s){
+long isNumber(const char* s)
+{
 	char* e = NULL;
    	long val = strtol(s, &e, 0);
    	if (e != NULL && *e == (char)0) return val; 
@@ -20,42 +24,52 @@ long isNumber(const char* s){
 
 
 ////////////////// CASE_H //////////////////
-static void case_h(){
+static void case_h()
+{
 	//apertura file
-	FILE *fp = fopen("help.txt", "r");
+	FILE *fp;
+	ec_null((fp = fopen("help.txt", "r")), "errore su fopen in case_h");
+    /*
      	//controllo esito apertura
-	if (!fp) {
+	if (!fp){
 		perror("fopen");
 		exit(EXIT_FAILURE);
-    	}
+    	*/
 	//lettura file in array di n char
-    	int n = 10;
+    	size_t n = 10;
 	char buf[n];
     	//scandisco il file fino alla fine
-	int temp;
-	while(!feof(fp) && (temp = fread(buf, sizeof(char), n, fp)) != 0 ){
-		for(int i = 0; i < temp; i++)
+	size_t temp;
+	while (!feof(fp) && (temp = fread(buf, sizeof(char), n, fp)) != 0 ){
+		for (int i = 0; i < temp; i++)
 			printf("%c", buf[i]);
   	}
-	fclose(fp);
+	if (fclose(fp) != 0){
+		perror("errore fclose in case_h"); 
+		exit(EXIT_FAILURE); 
+	}
 }
 
 
 ////////////////// IS_OPT //////////////////
-static int is_opt( char* arg, char* opt){
+static int is_opt( char* arg, char* opt)
+{
 	if(strcmp(arg, opt) == 0 && printf("opt %s riconosciuto\n", opt) ) return 1;
 	else return 0;
+	//elimina la stampa di opt -> qui solo per debug
 }
 
 ////////////////// IS_ARGUMENT //////////////////
-static int is_argument(char* c){
+static int is_argument(char* c)
+{
 	if (c == NULL || (*c == '-') ) return 0;
 	else return 1;
 }
 
 
 ////////////////// IS_ARGUMENT //////////////////
-void mystrtok_r (char* string){
+void mystrtok_r (char* string)
+{
 	//(!)problema: per ogni parola tokenizzata va effettuata una richiesta di scrittura al server
 	//si puo gestire direttamente da qui?
 	//rivaluta dopo l'implementazione dell'API
@@ -78,7 +92,8 @@ int is_directory(const char *path)
 }
 
 
-////////////////// VISIT_AND_REQUEST //////////////////
+
+////////////////// VISIT_FOLDER //////////////////
 void visit_folder_send_request(const char* dirname, int* n)
 {
 	//passo 1: apertura directory dirname
@@ -103,6 +118,24 @@ void visit_folder_send_request(const char* dirname, int* n)
 		}else{
 			//passo 4 caso 2: è un file
 			printf("richiesta di scrittura file: %s\n", entry->d_name);
+
+			//openFile/appendToFile/writeFile funzionamento:
+			//openFile apre un file -> il file puo essere gia presente sul server oppure va creato
+			//se esiste gia lo si scrive (aggiorna) con appendToFile
+			//se si tratta di un nuovo file lo si scrive in lock con writeFile
+
+			//verificare anche errno per distinguere i casi
+
+			if (openFile(entry->d_name, O_CREATE||O_LOCK) != 0){
+				perror("file esistente");
+				//file già esistente si scrive (aggiorna) con append
+				//(?) cosa voglio scrivere?
+				append_file(entry->d_name, arg_d)
+			}else{
+				ec_meno1(openFile(entry->d_name, O_LOCK), "openFile fallita");
+				//file aperto o creato in mod locked
+				writeFile(entry->d_name, arg_d);
+			}
 			(*n)--;
 		}
 	}
@@ -115,133 +148,193 @@ void visit_folder_send_request(const char* dirname, int* n)
 	ec_meno1(closedir(d), "errore su closedir");
 }
 
+////////////////// CASE_H //////////////////
 
-//////////////////PARSER //////////////////
+void case_w(char* arg_w)
+{
+		//in seguito in dirname salvo la directory contenuta in arg_w
+		char* dirname;
+		//verifica arg opzionale 
+		char* n = NULL;
+		int x;			
+		//strtok_r mi da il primo token appena dopo il primo delimitatore usando il puntatore n
+		strtok_r(arg_w, ",", &n);
+		//se n non è specificata -> x = -1
+		if (n == NULL){
+			x = -1;
+			dirname = arg_w;
+		}else{
+			dirname = strtok(arg_w, ",");
+			//controllo che sia un intero e che sia positivo
+			ec_meno1((x = isNumber(n)), "errore: arg. -w n non intero\n");
+			if (x < 0){ LOG_ERR(EINVAL, "arg -w optzionale non valido"); exit(EXIT_FAILURE); }
+			
+			//se uguale a zero come non fosse presente -> x = -1
+			if (x == 0) x = -1;
+		}
+		visit_folder(dirname, &x);
+}
+
+////////////////// PARSER //////////////////
 static void parser(int dim, char** array){
 
 	//il parsing è suddiviso in due cicli (si scandisce argv del main)
 
 	//CICLO 1: si gestiscono i comandi di setting -h, -f, -t, -p, -d, -D
-	char* arg_D = NULL;
-	char* arg_d = NULL;
+	
 	int i = 0;
 
 	while (++i < dim){
 		
 		//CASO -h / NON COMPLETO: gestire uscita con relative procedure di cleanu
-		if (is_opt(array[i], "-h")){
+		if (is_opt(array[i], "-h")) {
 			case_h();
 			exit(EXIT_FAILURE);
 		}
 		
 		//CASO-f
-		if (is_opt(array[i], "-f")){
+		if (is_opt(array[i], "-f")) {
 			//argomento obbligatorio - controlla se esiste
-			if (!is_argument(array[i+1])){ ec_meno1(-1, "argomento mancante"); }
-			else{
-				socket_name = array[++i];
-			}
+			if (!is_argument(array[i+1])){ 
+				LOG_ERR(EINVAL, "argomento -t mancante");
+				exit(EXIT_FAILURE); 
+			}else{ socket_name = array[++i]; }
 		}
 		
 		//CASO -t
-		if (is_opt(array[i], "-t")){
+		if (is_opt(array[i], "-t")) {
 			if ( !is_argument(array[i+1]) ) time_r = 0;
-			else ec_meno1( (time_r = isNumber(array[++i])), "argomento errato");
+			else ec_meno1( (time_r = isNumber(array[++i])), "argomento -t errato");
 		}	
 
 		//CASO -p
 		if (is_opt(array[i], "-p")) flag_p = 1;
 
 		//CASO -D
-		if (is_opt(array[i], "-D")){
+		if (is_opt(array[i], "-D")) {
 			flag_D = 1;
-			if (is_argument(array[i+1])){
-				arg_D = array[++i];
-				//CLEANUP
-			}
+			if (is_argument(array[i+1])) arg_D = array[++i];
 		}
 
-		//CASO -d
-		if (is_opt(array[i], "-d")){
-			if (is_argument(array[i+1])){
+		//CASO -d 
+		if (is_opt(array[i], "-d")) {
+			if (is_argument(array[i+1])) {
 				flag_d = 1;
 				arg_d = array[++i];
 				//CLEANUP
 			}else{
-				printf("errore: argomento -d mancante\n");
+				LOG_ERR(EINVAL, "argomento -d mancante");
+				exit(EXIT_FAILURE);
 			}
 		}
+
+		//controllo dipendenza -d da -w/-W e -D da -r/-R
+		if (is_opt(array[i], "-w") || is_opt(array[i], "-W")) flag_wW = 1;
+		if (is_opt(array[i], "-r") || is_opt(array[i], "-R")) flag_rR = 1;
 	}
+
+	//controllo dipendeza
+	if(flag_D && !flag_wW){ LOG_ERR(EINVAL, "-d attivo -w/-W non attivi"); exit(EXIT_FAILURE); }
+	if(flag_d && !flag_rR){ LOG_ERR(EINVAL, "-D attivo -r/-R non attivi"); exit(EXIT_FAILURE); }
 
 	//CICLO 2: si gestiscono i comandi di richiesta al server -w, -W, -R, -r, -l, -u, -c
 	i = 0;
-	while (++i < dim){	
-		//CASO -w
-		if (is_opt(array[i], "-w")){		
-			
-			//1) verifica -d/-D attivo altrimenti errore
-			if (!(flag_d || flag_D)){ 
-				printf("errore: -d / -D non attivi\n"); 
-			}else{
-				//2) verifica arg. obbligatorio
-				if (is_argument(array[i+1])){
-					char* arg_w = array[++i];
-					
-					//3) verifica arg opzionale 
-					char* n = NULL;
-					int x;
-					//strtok_r restituisce il primo token appena dopo il primo delimitatore
-					strtok_r(array[i], ",", &n);
-					//se n non è specificata
-					if (n == NULL) x = -1;
-					else {
-						//se n è un numero 
-						ec_meno1((x = isNumber(n)), "errore: arg. non intero\n");
-						//se zero è come non presente -> setto a -1
-						if(x == 0) x = -1; 
-					}
-					//CLEANUP
-					visit_folder_send_request(arg_w, &x);
 
-				}
-				else printf("errore: argomento -w mancante\n");
+	while (++i < dim){	
+		
+		//CASO -w
+		if (is_opt(array[i], "-w")) {		
+			//2) verifica arg. obbligatorio
+			if (is_argument(array[i+1])) {
+				case_w(array[++i]);
+			}else{ 
+				LOG_ERR(EINVAL, "argomento mancante -w");
+				exit(EXIT_FAILURE);
 			}	
 		}
 		
 		//CASO -W
-		if (is_opt(array[i], "-W")){
-			//1) verifica -d o -D attivo, altrimenti errore
-			if (!(flag_d || flag_D)){ printf("errore: -d / -D non attivi\n"); }
-			else{	
-				//argomento obbligatorio	
-				if (is_argument(array[i+1]))
-					mystrtok_r(array[++i]);
-				else printf("errore: argomento obligatorio\n");
+		if (is_opt(array[i], "-W")) {
+			//argomento obbligatorio	
+			if (is_argument(array[i+1]))
+				mystrtok_r(array[++i]);
+			else{
+				LOG_ERR(EINVAL, "argomento -W mancante");
+				exit(EXIT_FAILURE);
+			}
+		}
+		
+		//CASO -r
+		if (is_opt(array[i], "-r")) {
+			if (is_argument(array[i+1])) {
+				//altera la stringa?
+				//mystrtok_r(array[++i]);
+				//lista di nomi da leggere nel server
+				//richiesta di lettura al server per ogni file
+
+			}else{
+				LOG_ERR(EINVAL, "argomento -r mancante");
+				exit(EXIT_FAILURE);
+			}
+		}
+	
+
+		//CASO -R
+		if (is_opt(array[i], "-R")){
+			//puoi chiamare readfile o readnfile rispetto al paramentro n che riceve
+			//se n=0 o non presente legge tutti i file del server
+			//altrimenti ne legge n
+			//caso in cui n è presente: n=0 : n>0?
+			if (is_argument(array[i+1])) {
+				size_t x;
+				ec_meno1((x = isNumber(array[++i])), "errore: arg. -w n non intero\n");
+				if (x == 0) printf("rchiedere lettura di tutti i file nel server\n");
+				else printf("richiedere lettura di n file nel server\n");
+			}else{
+				//caso in cui n non è presente
+				printf("rchiedere lettura di tutti i file nel server\n");
+			}
+
+
+		}
+
+		//CASO -t
+		if (is_opt(array[i], "-t")){
+			//default: time_r=0 altrimenti time_r = x
+			if (is_argument(array[i+1]))
+				ec_meno1((time_r = isNumber(array[i+1])), "errore: arg. -w n non intero\n");
+		}
+
+		//CASO -l
+		if (is_opt(array[i], "-l")){
+			if (is_argument(array[i+1])){
+				printf("lista di nomi di file su cui acquisire la mutua esclusione\n");
+			}else{
+				LOG_ERR(EINVAL, "argomento -l mancante");
+				exit(EXIT_FAILURE);
 			}
 		}
 
-/*		
-		if (is_opt(array[i], "-r") && (flag_D || flag_d) ){
-			printf("opzione -r riconosciuta\n");
+		//CASO -u
+		if (is_opt(array[i], "-u")){
+			if (is_argument(array[i+1])){
+				printf("lista di nomi di file su cui rilasciare la mutua esclusione\n");
+			}else{
+				LOG_ERR(EINVAL, "argomento -u mancante");
+				exit(EXIT_FAILURE);
+			}
 		}
-		if (is_opt(array[i], "-R") == 0 && (flag_D || flag_d) ){
-			printf("opzione -R riconosciuta\n");
+
+		//CASO -c
+		if (is_opt(array[i], "-c")){
+			if (is_argument(array[i+1])){
+				printf("lista di file da rimuovere dal server se presenti\n");
+			}else{
+				LOG_ERR(EINVAL, "argomento -c mancante");
+				exit(EXIT_FAILURE);
+			}
 		}
-		if (is_opt(array[i], "-l") == 0  ){
-			printf("opzione -R riconosciuta\n");
-		}
-		if( strcmp((array[i]), "-l") == 0){
-			printf("opzione -L riconosciuta\n");
-			//argomento obligatorio
-		}
-		if( strcmp((array[i]), "-u") == 0 ){
-			printf("opzione -u riconosciuta\n");
-		}
-		if( strcmp((array[i]), "-c") == 0 ){
-			printf("opzione -c riconosciuta\n");
 		
-		//gestione comandi non riconosciuti?
-*/
 	}
 }
 
@@ -251,7 +344,7 @@ int main(int argc, char* argv[]){
 	
 	//controllo su argc
 	if( argc == 1){
-		ERR_H(EINVAL, "errore su argomenti\n");
+		LOG_ERR(EINVAL, "errore su argomenti\n");
 	}
 	
 	//parser
