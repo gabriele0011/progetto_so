da4abc680c09334a819b2a9d6a41d43871c2c947
gabriele0011 <99204202+gabriele0011@users.noreply.github.com>
2022-03-13 22:30:26 +0100

    Add files via upload

--- server_manager.c
+++ server_manager.c
@@ -1,12 +1,134 @@
-//iniziamo a definire lo scheletro del server manager nei suoi punti principali per poi andare ad affrontare 
-//un'implementazione piu profonda
 #include "err_cleanup.h"
 #include "util.h"
+#include <sys/socket.h>
+#include <sys/un.h>
 
+int err;
+
+//var. settate mediante file config.txt (!) aggiorna
 int t_workers_num = 0;
 int server_mem_size = 0;
 int server_file_size = 0;
-char* socket_file_name = NULL;
+char* socket_addr = NULL;
+
+#define UNIX_PATH_MAX 108
+
+//flags segnali di teminazione 
+//SIGINT/SIGQUIT: uscita prima possibile. non si accettano richieste, chiudere connessioni attive
+//SIG_HUP: non si accettano nuove connessioni e si termina una volta esaurite quelle attive
+int flag_sig_intquit = 0;
+int flag_sig_hup = 0;
+
+//prototipi di funzione
+void read_config_file(char* f_name);
+
+
+
+int main(int argc, char* argv[])
+{
+
+	//controllo argomenti main
+	if (argc <= 1){
+		LOG_ERR(EINVAL, "file config.txt mancante");
+		exit(EXIT_FAILURE);
+	}
+	
+	//parsing del file config.txt
+	if (read_config_file(argv[1]) != 0){
+		LOG_ERR(-1, "lettura file configurazione fallita");
+		exit(EXIT_FAILURE);
+	}
+
+
+	//STRUTTURE DATI
+
+	//buffer di lettura -> che dimensione ha?
+	int N = 256;
+	char buf[N];
+
+	//coda FIFO concorrente per comunicazione M -> Ws
+	t_queue conc_queue;
+	//if(conc_queue == NULL){ LOG_ERR(-1, "creazione coda conc. fallita"); exit(EXIT_FAILURE); }
+
+	//pipe senza nome per comunicazione Ws -> M
+	int pfd[2];
+	int fd_pipe_read = pfd[0];
+	int fd_pipe_write = pfd[1];
+	ec_meno1(pipe(pfd), "creazione pipe fallita");
+
+	//pool di thread Ws
+	pthread_t thread_workers_arr[t_workers_num];
+	for(int i = 0; i < t_workers_num; i++){
+		if (err = pthread_create(&(thread_workers_arr[i]), NULL, worker_func, NULL) != 0){    //worker_func?
+			LOG_ERR(err, "pthread_create fallita");
+			exit(EXIT_FAILURE);
+	}
+
+	//listen socket
+	struct sockaddr_un sa;
+	strncpy(sa.sun_path, socket_addr, UNIX_PATH_MAX);
+	sa.sun_family = AF_UNIX;
+	//dichiarazioni fd
+	int fd_skt, fd_c;	//fd socket e client
+	int fd_num;		//num. max fd attivi
+	fd_set set;		//insieme fd attivi
+	fd_set rdset;		//insieme fd attesi in lettura
+	//creazione listen socket
+	ec_meno1(fd_skt = socket(AF_UNIX, SOCK_STREAM, 0), "server socket() fallita");
+	ec_meno1(bind(fd_skt, (struct sockaddr_un*)sa, sizeof(*sa)), "bind() fallita");
+	ec_meno1(listen(fd_skt, SOMAXCONN), "listen() fallita");
+	//aggiornamento fd_num
+	if (fd_skt > fd_num) fd_num = fd_skt;
+	//inizializzazione e aggiornamento maschera
+	FD_ZERO(&set);		
+	FD_SET(fd_skt, &set);
+
+
+	//guardia del while da definire con controlli di terminazione signal
+	while(1){
+		rdset = set;
+		if (select(fd_num+1, &rdset, NULL, NULL, NULL) == -1){
+			LOG_ERR(-1, "select fallita");
+			exit(EXIT_FAILURE);
+		}else{
+			//scorre tra gli fd da testare
+			for (fd = 0; fd <= fd_num; fd++){
+				if (FD_ISSET(fd, &rdset)){ //trovato un fd pronto
+					
+					//si tratta del fd_skt (socket connect)
+					if (fd == fd_skt){ 
+						//si, stabilisci connessione con client (sovrascrive fd_c)
+						if ((fd_c = accept(fd_skt, NULL, 0)) == -1){
+							LOG_ERR(-1, "accept fallita");
+							exit(EXIT_FAILURE);
+						} 
+						fd_set(fd_c, &set);
+						if (fd_c > fd_num) fd_num = fd_c;
+
+
+					//se il fd pronto è un socket di I/O con client gia connesso
+					}else{	
+						nread = read(fd, buf, N);
+						//EOF client
+						if (nread == 0){	
+							FD_CLR(fd, &set);
+							fd_num = aggiorna(&set);
+							close(fd);
+
+						//socket di I/O attivo -> si puo comunicare	
+						}else{
+							printf("server got: %s\n", buf);
+							write(fd, "bye\n", 5);
+						}
+					}
+				}
+			}
+
+		}
+	}
+
+	return 0;
+}
 
 
 void read_config_file(char* f_name)
@@ -17,7 +139,7 @@ void read_config_file(char* f_name)
 
 
     	//allocazione buf[size] per memorizzare le righe del file
-    	int len = 512;
+    	int len = 256;
     	char s[len];
 
     	//acquisizione delle singole righe del file, mi aspetto in ordine:
@@ -47,10 +169,10 @@ void read_config_file(char* f_name)
 		if (strncmp(token1, "server_mem_size", len_t) == 0)
 			ec_meno1((server_mem_size = isNumber(string)), "parametro server_mem_size config.txt non valido");
 		
-		if (strncmp(token1, "socket_file_name", len_t) == 0 ){
-			socket_file_name = calloc(sizeof(char), len_string);
-			strncpy(socket_file_name, string, len_string);
-			socket_file_name[len_string] = '\0';
+		if (strncmp(token1, "socket_addr", len_t) == 0 ){
+			socket_addr = calloc(sizeof(char), len_string);
+			strncpy(socket_addr, string, len_string);
+			socket_addr[len_string] = '\0';
 		}
 	}
 
@@ -59,20 +181,5 @@ void read_config_file(char* f_name)
 		LOG_ERR(errno, "errore fclose in read_config_file");
 		exit(EXIT_FAILURE);
 	}
-}
-
-
-
-int main(int argc, char* argv[])
-{
-
-	//controllo sui parametri in ingresso -> mi aspetto un argomento, ovvero il file config.txt
-	if(argc < 1){
-		LOG_ERR(EINVAL, "file config.txt mancante");
-		exit(EXIT_FAILURE);
-	}
-	//apertura del file config.txt da parsare per ottenere le informazioni di setup del server
-	read_config_file(argv[1]);
-
 	return 0;
 }
