f5b0b41b0d0fcf9828007c6e6ef1057dd385b33b
gabriele0011 <99204202+gabriele0011@users.noreply.github.com>
2022-07-06 15:32:19 +0200

    Add files via upload

--- server_manager.c
+++ server_manager.c
@@ -1,6 +1,4 @@
 //1 ricontrollare la terminazione dei segnali
-//2. 
-
 
 #include "server_manager.h"
 #define O_CREATE 10
@@ -61,14 +59,14 @@ int read_config_file(char* f_name)
 		//quarta e ultima acquisizione non ha '\n'
 		if (strncmp(token1, "socket_file_name", len_t) == 0 ){
 			int len_token2 = strlen(token2);
-			socket_file_name = calloc(sizeof(char), len_token2);
-			strncpy(socket_file_name, token2, len_token2);
+			socket_name = calloc(sizeof(char), len_token2);
+			strncpy(socket_name, token2, len_token2);
 			//socket_file_name[len_string] = '\0';
 		}
 		n++;
 	}
 	fclose(fd);
-	fprintf(stderr, "ho letto config.txt: %s\n", socket_file_name);
+	fprintf(stderr, "ho letto config.txt: %s\n", socket_name);
 	return 0;
 }
 
@@ -80,33 +78,33 @@ static void handler_sigintquit(int signum){
 static void handler_sighup(int signum){
 	sig_hup = 1;
 }
+
+//start function thread di prova
 void* start_func2(void *arg){
+      printf("start_func2 debug\n");
 }
 
 //MAIN
 int main(int argc, char* argv[])
 {
-
 	int err;
-	//controllo argomenti main
-	if (argc <= 1){
+	if (argc < 1){
 		LOG_ERR(EINVAL, "server_manager: file config.txt mancante");
 		exit(EXIT_FAILURE);
 	}
 	
-	//PARSING config.txt
+	/************** LETTURA CONFIG.TXT **************/
 	if (read_config_file(argv[1]) != 0){
 		LOG_ERR(-1, "server_manager: lettura file configurazione fallita");
 		exit(EXIT_FAILURE);
 	}
 
-	//SEGNALI 
+	/************** SEGNALI **************/
 	struct sigaction s;
 	//memset((&s, 0, sizeof(s));
 	s.sa_handler = handler_sigintquit;
 	ec_meno1(sigaction(SIGINT, &s, NULL), "server_manager: sigaction fallita");
 	
-
 	struct sigaction s1;
 	s1.sa_handler = handler_sigintquit;
 	ec_meno1(sigaction(SIGQUIT, &s1, NULL), "server_manager: sigaction fallita");
@@ -115,22 +113,22 @@ int main(int argc, char* argv[])
 	s2.sa_handler = handler_sighup;
 	ec_meno1(sigaction(SIGQUIT, &s2, NULL), "server_manager: sigaction fallita");
 	
-	//CACHE
+	/************** CACHE **************/
 	create_cache(server_mem_size, max_storage_file);
+      //printf("server.DEBUG: creazione cache riusciata\n");
 
-	//STRUTTURE DATI
-	/*********** buf lettura ***********/
+	//BUF LETTURA
 	int buf;
 
-	/*********** pipe senza nome ***********/
+	/************** PIPE SENZA NOME **************/
 	int pfd[2];
+	ec_meno1(pipe(pfd), "server_manager: creazione pipe fallita");
 	int fd_pipe_read = pfd[0];
 	int fd_pipe_write = pfd[1];
-	ec_meno1(pipe(pfd), "server_manager: creazione pipe fallita");
-	printf("Pipe read fd: %d\n", fd_pipe_read);
-    	printf("Pipe write fd: %d\n\n", fd_pipe_write);
+      //printf("Pipe write fd: %d\n\n", fd_pipe_write);
+	//printf("Pipe read fd: %d\n", fd_pipe_read);
 	
-	/*********** thread pool ***********/
+      /************** THREAD POOL  **************/
 	pthread_t thread_workers_arr[t_workers_num];
 	for(int i = 0; i < t_workers_num; i++){
 		if ((err = pthread_create(&(thread_workers_arr[i]), NULL, start_func, NULL)) != 0){    
@@ -139,35 +137,35 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	/*********** listen socket ***********/
-	int lenlen = strlen(socket_file_name)+1;
-	socket_file_name[lenlen] = '\0';
-	
-	struct sockaddr_un sa;
-	strncpy(sa.sun_path, socket_file_name, strlen(socket_file_name));
-	sa.sun_family = AF_UNIX;
-	//dichiarazioni fd
+      /**************  DICHIARAZIONE FD **************/
 	int fd_skt, fd_c;	//fd socket e client
 	int fd_num = 0;		//num. max fd attivi
-	fd_set set;		//insieme fd attivi
+	fd_set set;			//insieme fd attivi
 	fd_set rdset;		//insieme fd attesi in lettura
-	
-	//printf("sun path %s\n", sa.sun_path);
-	//printf("socket address: %s\n", socket_file_name);
-	//creazione listen socket
-	fprintf(stderr, "DEBUG: %s\n", socket_file_name);
-	ec_meno1((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)), "server_manager: socket() fallita");
+
+	/************** LISTEN SOCKET **************/
+	int l = strlen(socket_name)+1; //non necessario
+	socket_name[l] = '\0';         //non necessario
+      struct sockaddr_un sa;
+	strncpy(sa.sun_path, socket_name, l);
+	sa.sun_family = AF_UNIX;
+      //printf("socket_name: %s / strlen(socket_name) = %zu / l = %d\n", socket_name, strlen(socket_name), l);
+      //printf("sa.sun_path: %s / strlen(sa.sun_path) = %zu\n", sa.sun_path, strlen(sa.sun_path));
+
+      //socket/bind/listen
+      ec_meno1((fd_skt = socket(AF_UNIX, SOCK_STREAM, 0)), "server_manager: socket() fallita");
 	ec_meno1(bind(fd_skt, (struct sockaddr*)&sa, sizeof(sa)), "server_manager: bind() fallita");
 	ec_meno1(listen(fd_skt, SOMAXCONN), "server_manager: listen() fallita");
 	printf("server in ascolto\n");
-	//aggiornamento fd_num
+	
+      //aggiornamento fd_num
 	if (fd_skt > fd_num) fd_num = fd_skt;
 	//inizializzazione e aggiornamento maschera
 	FD_ZERO(&set);		
 	FD_SET(fd_skt, &set);
-
-   	int n_client_conn = 0;
-
+      int n_client_conn = 0;
+	
+      /************** LOOP **************/
 	while (!sig_intquit){
 		if(sig_hup && n_client_conn == 0){
 			printf("SIG_HUP: connessione terminate, uscita\n");
@@ -182,66 +180,57 @@ int main(int argc, char* argv[])
 			//controlla fd intercettati da select
 			for (int fd = 0; fd <= fd_num; fd++){
 				if (FD_ISSET(fd, &rdset)){
-					//printf("intercettato fd nuov = %d\n",fd);
-					//caso 1: si tratta del fd_skt
+					printf("intercettato fd = %d\n",fd);
+					//CASO 1: si tratta del fd_skt -> tentativo di connessione da parte di un client
 					if (fd == fd_skt){ 
-						printf("fd = %d è il fd socket\n",fd);
+						printf("fd = %d è il fd socket connect\n\n", fd);
 						//CONTROLLO SEGNALE sig_hup
 						if (!sig_hup){
-							//stabilisci connessione con un client
-							if ((fd_c = accept(fd_skt, NULL, 0)) == -1){
-								LOG_ERR(-1, "server_manager: accept fallita");
-								break;
-							}
-							printf("fd_client %d accettato\n",fd_c); 
+							//stabilisci connessione con un client ridirigendolo sulla nuova socket dedicata
+							if ((fd_c = accept(fd_skt, NULL, 0)) == -1){ LOG_ERR(-1, "server_manager: accept fallita"); break; }
 							FD_SET(fd_c, &set);
 							if (fd_c > fd_num) fd_num = fd_c;
+                                          printf("DEBUG.loop: fd_c = %d aggiunto nel rdset\n", fd_c);
 						}
 					}else{ 
-							
-						//caso 2: si tratta del fd della pipe -> un thread ha un messaggio
+						//caso 2: si tratta del fd della pipe (I/O con client) -> un thread ha un messaggio
 						if (fd == fd_pipe_read){
-							if(read(fd_pipe_read, &buf, sizeof(int)) == -1){
-								LOG_ERR(-1, "server_manager: readn fallita");
-								break;
-							}
+							if (read(fd_pipe_read, &buf, sizeof(int)) == -1){ LOG_ERR(-1, "server_manager: readn fallita"); break; }
+							//si hanno due casi, rispettivamente due messaggi possibili:
 							//richiesta servita -> thread ritorna fd
-							if( buf == fd ) FD_SET(buf, &set);
+							if (buf == fd) FD_SET(buf, &set);
 							//client disconnesso (read legge 0) -> thread ritorna -fd
-							if(buf <= 0){
+							if (buf <= 0){
 								buf = buf*(-1);
 								FD_CLR(buf, &set);
 								close(fd);
 								n_client_conn--;
-							}	
+							}
 						//caso 3: inserisce fd client (connesso)in coda concorrente -> nuova richiesta
-						}else{	
+						}else{
+                                          printf("sto servendo fd = %d\n", fd);
 							//lock
 							mutex_lock(&g_mtx, "server_manager: lock fallita");
-							//inserisci
-							if(enqueue(&conc_queue, fd) == -1){
-								LOG_ERR(err, "server_manager: enqueue fallita");
-								break;
-							}
+							//inserisci richiesta in coda
+							if(enqueue(&conc_queue, fd) == -1){ LOG_ERR(err, "server_manager: enqueue fallita"); break; }
 							//segnale
-							if ((err = pthread_cond_signal(&cv)) == -1){
-								LOG_ERR(err, "server_manager: signal error");
-								break;
-							}
-							printf("nuova richiesta in coda da client %d\n",fd );
+							if ((err = pthread_cond_signal(&cv)) == -1){ LOG_ERR(err, "server_manager: signal error"); break; }
+							printf("nuova richiesta da %d inserita in coda\n",fd );
 							//unlock
 							mutex_unlock(&g_mtx, "server_manager: unlock fallita");
-							n_client_conn++;
-							printf("n_client_conn = %d\n", n_client_conn);
+							
+							n_client_conn++; 
+                                          printf("n_client_conn = %d\n", n_client_conn);
 							FD_CLR(fd, &set);
 							fd_num--;
-							close(fd);
+							close(fd);	
 						}
 					}
 				}
 			}
 		}
 	}
+
 	/*********** chiusura server ***********/
 	for (int i = 0; i < t_workers_num; i++) {
 		if ((err = pthread_join(thread_workers_arr[i], NULL)) == -1){
@@ -258,7 +247,7 @@ int main(int argc, char* argv[])
 	exit(0);
 
 	//cleanup_section -> (!) eliminabile?
-	cleanup_section:
+	//cleanup_section:
 
 	if(conc_queue) dealloc_queue(conc_queue);
 	close(fd_pipe_read);
@@ -268,12 +257,13 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+
 void* start_func(void *arg)
 {
 	int* buf = NULL;
 	ec_null((buf = malloc(sizeof(int))), "start_func: malloc fallita");
 	*buf = 0;
-	int fd_client;
+	int fd_c;
 	int op;
 	int err;
 	
@@ -281,38 +271,36 @@ void* start_func(void *arg)
 		mutex_lock(&g_mtx, "start_func: lock fallita");
 		//pop richiesta dalla coda concorrente
 		while (((*buf = dequeue(&conc_queue)) == -1) && !sig_intquit){
-			//wait
+                  printf("DEBUG.start_func: buf dequeue = %d\n", *buf); //DEBUG DEBUG DEBUG
+                  //wait
 			if ( (err = pthread_cond_wait(&cv, &g_mtx)) == -1){
 				LOG_ERR(err, "start_func: phtread_cond_wait fallita");
 				exit(EXIT_FAILURE);
 			}
 		}
 		mutex_unlock(&g_mtx, "start_func: lock fallita");
-		//salvo il client
-		fd_client = *buf;
-		//libero il buffer e rialloco per riasarlo in lettura
-		if (buf){ 
-			free(buf); 
-			buf = NULL; 
-		}
-		ec_null((buf = malloc(sizeof(int))), "start_func: malloc fallita");
+		
+            
+            //salvo il client
+		fd_c = *buf;
 		*buf = 0;
+            printf("DEBUG.start_func: buf dequeue = %d\n", *buf); //DEBUG DEBUG DEBUG
 
 		//leggi la richiesta dal client
-		if (read(fd_client, buf, sizeof(int)) == -1){
+		if (read(fd_c, buf, sizeof(int)) == -1){
 			LOG_ERR(-1, "start_func: read su client");
 			*buf = 0;
 		}
 
-		//a seguire codifica di op e identificazione del comando
 		op = *buf;
-		printf("richiesta di tipo %d dal client %d\n", op, fd_client);
 
+            //DEBUG: cosa accade se op == EOF? si entra comunque nel caso default dello switch?
+		
 		//client disconnesso
 		if (op == EOF){
-			fprintf(stderr, "client %d disconnesso\n", fd_client);
-			*buf = fd_client;
-			*buf = fd_client*(-1);
+			fprintf(stderr, "start_func: client %d disconnesso\n", fd_c);
+			*buf = fd_c;
+			*buf = fd_c*(-1);
 			if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) != -1)
 				LOG_ERR(-1, "start_func: write su pipe fallita");
 		}
@@ -320,126 +308,126 @@ void* start_func(void *arg)
 		switch(op){
 			//client disconnesso
 			case 0:
-				fprintf(stderr, "client %d disconnesso\n", fd_client);
-				*buf = fd_client;
-				*buf = fd_client*(-1);
+				fprintf(stderr, "start_func: (0) client %d disconnesso\n", fd_c);
+				*buf = fd_c;
+				*buf = fd_c*(-1);
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (0) write su pipe fallita");
 				break;
 			//openFile
 			case 1:
-				if (worker_openFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_openFile fallita");
+				if (worker_openFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (1) worker_openFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				printf("op %d dal client %d terminata\n", op, fd_client);
-				*buf = fd_client;
+				printf("op %d dal client %d terminata\n", op, fd_c);
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (1) write su pipe fallita");
 				break;
 			
 			//closeFile
 			case 2:
-				if (worker_closeFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_closeFile fallita");
+				if (worker_closeFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (2) worker_closeFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (2) write su pipe fallita");
 				break;
 			
 			//writeFile
 			case 3:
-				if (worker_writeFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_writeFile fallita");
+				if (worker_writeFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (3) worker_writeFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (3) write su pipe fallita");
 				break;
 			
 			//appendToFile
 			case 4:
-				if (worker_appendToFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_appendToFile fallita");
+				if (worker_appendToFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (4) worker_appendToFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (4) write su pipe fallita");
 				break;
 			
 			//readFile
 			case 5:
-				if (worker_readFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_readFile fallita");
+				if (worker_readFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (5) worker_readFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (5) write su pipe fallita");
 				break;
 			
 			//readNFiles
 			case 6: 
-				if (worker_readNFiles(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_readNFiles fallita");
+				if (worker_readNFiles(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (6) worker_readNFiles fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (6) write su pipe fallita");
 				break;
 			
 			//lockFile
 			case 7: 
-				if (worker_lockFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_lockFile fallita");
+				if (worker_lockFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (7) worker_lockFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (7) write su pipe fallita");
 				break;
 			
 			//unlockFile
 			case 8: 
-				if (worker_unlockFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_unlockFile fallita");
+				if (worker_unlockFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (8) worker_unlockFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (8) write su pipe fallita");
 				break;
 			
 			//removeFile
 			case 9: 
-				if (worker_removeFile(fd_client) == -1){
-					LOG_ERR(-1, "server_worker: worker_removeFile fallita");
+				if (worker_removeFile(fd_c) == -1){
+					LOG_ERR(-1, "start_func: (9) worker_removeFile fallita");
 					exit(EXIT_FAILURE);
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (9) write su pipe fallita");
 				break;
 			//default
 			default:
  				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1){
-					LOG_ERR(EPIPE, "server_worker: write su pipe fallita");
+					LOG_ERR(EPIPE, "start_func: (default) write su pipe fallita");
 				}
-				*buf = fd_client;
+				*buf = fd_c;
 				if (write(fd_pipe_write, buf, PIPE_MAX_LEN_MSG) == -1)
-					LOG_ERR(EPIPE, "server_worker: richiesta client non valida");
+					LOG_ERR(EPIPE, "start_func: (default) richiesta client non valida");
 				break;
 		}
 	}
+      printf("richiesta di tipo %d dal client %d\n", op, fd_c);
 	if(buf) free(buf);
 	pthread_exit((void*)0);
 }
-
 
 static int worker_openFile(int fd_client)
 {
@@ -475,6 +463,5 @@ static int worker_openFile(int fd_client)
 
 	printf("pathname: %s flags = %d\n", pathname, flags);
 	return 0;
-
-
 }
+
